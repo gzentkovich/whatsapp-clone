fd362b5360d64ef5013448ae91a7936929a73e40
G. Zentkovich <gzentkovich@gmail.com>
2018-02-04 07:56:38 -1000

    load chat controller

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -1,6 +1,7 @@
 // Libs
 import 'angular-animate';
 import 'angular-meteor';
+import 'angular-moment';
 import 'angular-sanitize';
 import 'angular-ui-router';
 import 'ionic-scripts';
@@ -10,6 +11,7 @@ import { Meteor } from 'meteor/meteor';
 
 // Modules
 import ChatsCtrl from '../controllers/chats.controller';
+import ChatCtrl from '../controllers/chat.controller';
 import CalendarFilter from '../filters/calendar.filter';
 import RoutesConfig from '../routes';
 
@@ -18,11 +20,13 @@ const App = 'Whatsapp';
 // App
 Angular.module(App, [
   'angular-meteor',
+  'angular-moment',
   'ionic'
 ]);
 
 new Loader(App)
   .load(ChatsCtrl)
+  .load(ChatCtrl)
   .load(CalendarFilter)
   .load(RoutesConfig);
 
